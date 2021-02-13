0cb2d2227fbe2f6267fb09cf4b0fa0833476479b
al119c <example@gmail.com>
2021-02-13 23:17:49 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    let arr = n.toString().split("");
+    if (arr[0] != '-') {
+        return arr.reverse().join("");
+    } else {
+        newArr = arr.splice(1, 3);
+        return newArr.reverse().join("");
+    }
+};
